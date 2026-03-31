dbb21a2c882d66e48b3620bd80125acee258bc68
peng.chen <vitoc@qq.com>
2026-04-01 01:15:41 +0800

    fix

--- app/src/main/java/org/screenlite/webkiosk/components/WebViewComponent.kt
+++ app/src/main/java/org/screenlite/webkiosk/components/WebViewComponent.kt
@@ -25,6 +25,7 @@ import org.screenlite.webkiosk.data.KioskSettingsFactory
 import org.screenlite.webkiosk.data.Rotation
 import androidx.compose.ui.platform.LocalConfiguration
 import android.content.res.Configuration
+import android.webkit.JavascriptInterface
 
 private const val TAG = "WebViewComponent"
 
