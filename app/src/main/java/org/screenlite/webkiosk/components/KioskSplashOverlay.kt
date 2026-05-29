be7858ab01c393509e3f895e82007ebcbe29f79d
peng.chen <vitoc@qq.com>
2026-05-29 23:03:02 +0800

    fix

--- app/src/main/java/org/screenlite/webkiosk/components/KioskSplashOverlay.kt
+++ app/src/main/java/org/screenlite/webkiosk/components/KioskSplashOverlay.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
@@ -19,10 +20,11 @@ fun KioskSplashOverlay(modifier: Modifier = Modifier) {
             .background(colorResource(R.color.splash_screen_background))
     ) {
         Image(
-            painter = painterResource(R.drawable.splash_background),
+            painter = painterResource(R.drawable.welcome),
             contentDescription = null,
             modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.FillBounds,
+            alignment = Alignment.Center
         )
     }
 }
