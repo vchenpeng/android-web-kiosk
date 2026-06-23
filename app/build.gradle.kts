76a84279cf4dee8866b7b64f772724246833c207
peng.chen <vitoc@qq.com>
2026-06-23 12:03:27 +0800

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jianyitong.robot"
         minSdk = 22
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.0.12"
+        versionCode = 13
+        versionName = "0.0.13"
     }
 
     signingConfigs {
