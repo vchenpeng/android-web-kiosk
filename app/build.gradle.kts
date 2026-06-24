b398391520eaa2742f16a6c95a9d24fc2c01eed4
peng.chen <vitoc@qq.com>
2026-06-24 15:35:47 +0800

    feat: v1.0.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jianyitong.robot"
         minSdk = 22
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.0.15"
+        versionCode = 16
+        versionName = "0.0.16"
     }
 
     signingConfigs {
